4bd957616132928e1824e2237d9809e6ebfc40a9
Vishu Yadav <vishuyadav0109@gmail.com>
2026-01-25 21:45:19 +0530

    Update isAuthenticated.js

--- Backend/middleware/isAuthenticated.js
+++ Backend/middleware/isAuthenticated.js
@@ -6,7 +6,7 @@ const authenticateToken = (req, res, next) => {
 
     if (!token) {
       return res.status(401).json({
-        message: "No token provided",
+        message: "Please Login First",
         success: false,
       });
     }
