a2881d776615c7a5df1928698eacf56b0910aeff
Vishu Yadav <vishuyadav0109@gmail.com>
2026-01-25 20:59:30 +0530

    Fix CORS options URL by removing trailing slash

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://job-portal-t692.vercel.app/"
+    "https://job-portal-t692.vercel.app"
   ],
   credentials: true,
 };
